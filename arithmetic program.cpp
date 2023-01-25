f4b721de72f5b1c9c4de6fd36810af55bf52576d
rishigithub001 <123451526+rishigithub001@users.noreply.github.com>
2023-01-25 13:55:21 +0530

    Add files via upload

---
+++ arithmetic program.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,p;
+    int op;
+    printf(" 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division\n 5.Power\n");
+    printf("Enter the values of a & b: ");
+    scanf("%d %d",&a,&b);
+    printf("Enter your Choice : ");
+    scanf("%d",&op);
+    switch(op)
+    {
+    case 1	:
+        printf("Sum of %d and %d is : %d",a,b,a+b);
+        break;
+    case 2	:
+        printf("Difference of %d and %d is : %d",a,b,a-b);
+        break;
+    case 3	:
+        printf("Multiplication of %d and %d is : %d",a,b,a*b);
+        break;
+    case 4	:
+        printf("Division of Two Numbers is %d : ",a/b);
+        break;
+    case 5 :
+    	p=pow(a,b);
+    	printf("Power of %d and %d is %d;",a,b,p)-;
+    	break;
+    default	:
+        printf(" Enter Your Correct Choice.");
+        break;
+    }
+    return 0;
+}
+
