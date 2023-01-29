7cb32964c990fec5030dee957cce0349dd4a7c1a
rishigithub001 <123451526+rishigithub001@users.noreply.github.com>
2023-01-29 23:16:23 +0530

    Add files via upload

--- Int prime or not.cpp
+++ Int prime or not.cpp
@@ -1,25 +1,23 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+int main() 
 {
-int n,i,flag=0;
-printf("Enter a positive integer:");
-scanf("%d",&n);
-if(n==0||n==1)
-flag=1;
-for(i=2;i<=n/2;++i)
+int n, i, flag = 0;
+printf("Enter a positive integer: ");
+scanf("%d", &n);
+if (n == 0 || n == 1)
+flag = 1;
+for (i = 2; i <= n / 2; ++i) 
 {
-if(n%1==0)
+if (n % i == 0) 
 {
-flag =1;
+flag = 1;
 break;
 }
 }
-if (flag==0)
-{
-printf("%d is a non-prime number.",x);
-}
+if (flag == 0)
+printf("%d is a prime number.", n);
 else
-{
-printf("%d is a prime number.",x);
-}
+printf("%d is not a prime number.", n);
+return 0;
 }
